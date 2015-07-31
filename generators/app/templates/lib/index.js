cc515fbe1a4018fcb60b9ae502b6dcddf29fe8c0
James Messinger <james.messinger@gmail.com>
2015-07-31 17:16:13 -0500

    version banner

--- generators/app/templates/lib/index.js
+++ generators/app/templates/lib/index.js
@@ -1,3 +1,9 @@
+/**!
+ * <%= project.friendlyName %> v0.0.1
+ *
+ * @link https://github.com/<%= options.githubUsername.toLowerCase() %>/<%= project.name %>
+ * @license MIT
+ */
 'use strict';
 
 module.exports.sayHello = function() {
