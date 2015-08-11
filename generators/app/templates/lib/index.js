9dc70c1b4aa57703b871a53981ba83aea205b708
James Messinger <james.messinger@gmail.com>
2015-08-11 02:37:55 -0500

    Only include the version-number header for browser-based projects

--- generators/app/templates/lib/index.js
+++ generators/app/templates/lib/index.js
@@ -1,9 +1,11 @@
+<% if (options.env.browser) { -%>
 /**!
  * <%= project.friendlyName %> v0.0.1
  *
  * @link https://github.com/<%= options.githubUsername.toLowerCase() %>/<%= project.name %>
  * @license MIT
  */
+<% } -%>
 'use strict';
 
 module.exports.sayHello = function() {
